59d8d5ec748a204b38622bcc2163a4ba1074cf59
rts-Liva <U2451012@rts-365.dk>
2025-09-22 15:56:24 +0200

    search functionality

--- projekt/src/components/forms/search-form.jsx
+++ projekt/src/components/forms/search-form.jsx
@@ -1,29 +1,38 @@
 'use client'
 
-import { useContext, useEffect } from "react";
+import { useActionState, useContext, useEffect } from "react";
 import { listingsContext } from "../providers/listings-provider";
 import { FiSearch } from "react-icons/fi";
+import searchAction from "./search-action";
 import '@/scss/components/search.scss';
 
-function SearchForm({ listings }) {
+function SearchForm() {
     const { setResults } = useContext(listingsContext);
+    const [formState, formAction, isPending] = useActionState(searchAction);
 
     useEffect(() => {
-        setResults(listings.slice(0, 6));
-    }, []);
+        if (!formState || !formState.success) return;
+        console.log(formState);
+
+    }, [formState]);
+
 
     return (
-        <form className="search">
-            <div className="search-field">
-                <label>
-                    <input
-                        type="search"
-                        name="query"
-                        autoComplete="off"
-                        placeholder="Search"
-                        className="search-field__input" />
-                </label>
-                <button type="submit" className="search-field__icon"><FiSearch /></button>
+        <form action={formAction} className="search">
+            <div>
+                <div className="search-field">
+                    <label>
+                        <input
+                            type="search"
+                            name="query"
+                            autoComplete="off"
+                            placeholder="Search"
+                            defaultValue={formState?.query}
+                            className="search-field__input" />
+                    </label>
+                    <button type="submit" className="search-field__icon"><FiSearch /></button>
+                </div>
+                <span className="search__error">{formState?.properties?.query?.errors}</span>
             </div>
             <div className="search-filter">
                 <label>
